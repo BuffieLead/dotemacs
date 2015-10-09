2085e74d0a05841cedc177cc6e8482176c70423a
Toshiyuki-Yamashita <toshiyuki.yamashita@konicaminolta.com>
2015-10-09 19:28:40 +0900

    package.el

--- .emacs.d/auto-install.el
+++ .emacs.d/auto-install.el
@@ -2,10 +2,17 @@
 ;; -*- mode: emacs-lisp; coding: utf-8; indent-tabs-mode: nil -*-
 ;; $Id: auto-install.el 123 2015-03-19 04:46:09Z e09485 $
 
-;(require 'package)
-;(setq package-user-dir "~/.emacs.d/elisp/package/")
-;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;(package-initialize)
+(require 'package)
+(setq package-user-dir "~/.emacs.d/elisp/package/")
+;; MELPAを追加
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; MELPA-stableを追加
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; Marmaladeを追加
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; Orgを追加
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
 
 ; melpa.el
 ;(require 'melpa)
