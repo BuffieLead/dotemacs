7f7ea228eece3f409c6a0420ccfad91ca40073ef
Toshiyuki-Yamashita <toshiyuki.yamashita@konicaminolta.com>
2016-02-22 15:22:32 +0900

    puml config

---
+++ .emacs.d/puml-mode.el
@@ -0,0 +1,32 @@
+;;; puml-mode.el --- puml-mode config                -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  
+
+;; Author:  <e09485@DD70656651062A>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(setq puml-plantuml-jar-path (expand-file-name "~/bin/java/plantuml.jar"))
+;; Enable puml-mode for PlantUML files
+(add-to-list 'auto-mode-alist '("\\.puml\\'" . puml-mode))
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . puml-mode))
+
+;;; puml-mode.el ends here
