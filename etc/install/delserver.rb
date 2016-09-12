09580714d12767e421994b529bb5f6b3f82effc6
Toshiyuki-Yamashita <Toshiyuki-Yamashita@users.noreply.github.com>
2016-09-12 12:35:58 +0900

    レジストリから削除対象を取得

--- etc/install/delserver.rb
+++ etc/install/delserver.rb
@@ -2,12 +2,25 @@
 # ocra --chdir-first --windows delserver.rb
 # $Id: delserver.rbw 107 2014-07-03 04:39:44Z e09485 $
 require 'rubygems'
+require 'win32/registry'
 
-Prog =  $0
+def regkey(path, key)
+    Win32::Registry::HKEY_CURRENT_USER.open(path) do |reg|
+        yield reg['HOME'].gsub( '\\', '/' )
+    end
+end
 
-ScriptDir = File.dirname(File.expand_path(Prog))
-RootDir = File.expand_path("#{ScriptDir}/../..")
-ServerPath = "#{RootDir}/.emacs.d/server/server"
+HOME = 'HOME'
+Path = 'Software\GNU\emacs'
+
+emacsdir = ENV[HOME]
+regkey(Path, HOME) {
+    |val| emacsdir = val unless val.nil?
+}
+
+puts emacsdir
+
+ServerPath = "#{emacsdir}/.emacs.d/server/server"
 File.delete(ServerPath) if File.exists? ServerPath
 
 
